fee628b1688a71fc6d9a1c68dc1d51bbeb7a11b4
Malu Silva <malursilva1712@gmail.com>
2020-12-15 23:39:01 -0300

    test: validate emails with more than 320 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -1,23 +1,28 @@
-import { Email } from "./email"
+import { Email } from './email'
 
-describe ('Email validation', () => {
-    test('should not accept null strings', () => {
-        const email = null
-        expect(Email.validate(email)).toBeFalsy
-    })
+describe('Email validation', () => {
+  test('should not accept null strings', () => {
+    const email = null
+    expect(Email.validate(email)).toBeFalsy()
+  })
 
-    test('should not accept empty strings', () => {
-        const email = ''
-        expect(Email.validate(email)).toBeFalsy
-    })
+  test('should not accept empty strings', () => {
+    const email = ''
+    expect(Email.validate(email)).toBeFalsy()
+  })
 
-    test('should accept valid email', () => {
-        const email = 'any@mail.com'
-        expect(Email.validate(email)).toBeTruthy
-    })
+  test('should accept valid email', () => {
+    const email = 'any@mail.com'
+    expect(Email.validate(email)).toBeTruthy()
+  })
 
-    test('should not accept local part larger than 64 chars', () => {
-        const email = 'l'.repeat(65) + '@mail.com'
-        expect(Email.validate(email)).toBeFalsy
-    })
+  test('should not accept strings larger than 320 chars', () => {
+    const email = 'l'.repeat(65) + '@' + 'd'.repeat(128) + '.' + 'd'.repeat(127)
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should not accept local part larger than 64 chars', () => {
+    const email = 'l'.repeat(65) + '@mail.com'
+    expect(Email.validate(email)).toBeFalsy()
+  })
 })
