f8e05c725a70b530594a35856b44f403f3f80c3d
Malu Silva <malursilva1712@gmail.com>
2020-12-15 23:38:17 -0300

    feat: validate emails with more than 320 chars

--- src/entities/Email.ts
+++ src/entities/Email.ts
@@ -1,15 +1,19 @@
 export class Email {
-    static validate (email: string): boolean {
-        if (!email) {
-            return false
-        }
-        
-        const [local, domain] = email.split('@')
-
-        if (local.length > 64) {
-            return false
-        }
-        
-        return true
+  static validate (email: string): boolean {
+    if (!email) {
+      return false
     }
+
+    if (email.length > 320) {
+      return false
+    }
+
+    const [local] = email.split('@')
+
+    if (local.length > 64) {
+      return false
+    }
+
+    return true
+  }
 }
