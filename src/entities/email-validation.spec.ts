fcadbcefe37d766d2fca1798d6c3fd925c114baf
Malu Silva <malursilva1712@gmail.com>
2020-12-15 23:29:45 -0300

    feat: validate local part larger than 64 chars

--- src/entities/email-validation.spec.ts
+++ src/entities/email-validation.spec.ts
@@ -1,18 +1,23 @@
 import { Email } from "./email"
 
 describe ('Email validation', () => {
-    test ('should not accept null strings', () =>{
+    test('should not accept null strings', () => {
         const email = null
         expect(Email.validate(email)).toBeFalsy
     })
 
-    test ('should not accept empty strings', () =>{
+    test('should not accept empty strings', () => {
         const email = ''
         expect(Email.validate(email)).toBeFalsy
     })
 
-    test ('should accept valid email', () =>{
+    test('should accept valid email', () => {
         const email = 'any@mail.com'
         expect(Email.validate(email)).toBeTruthy
+    })
+
+    test('should not accept local part larger than 64 chars', () => {
+        const email = 'l'.repeat(65) + '@mail.com'
+        expect(Email.validate(email)).toBeFalsy
     })
 })
