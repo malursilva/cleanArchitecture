d112799ad9826e52eb2ab0365686f224940a0578
Malu Silva <malursilva1712@gmail.com>
2020-12-23 22:50:49 -0300

    feat: create valid user

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,20 +1,33 @@
 import { UserData } from './user-data'
-import { Either, left } from '../shared/either'
+import { Either, left, right } from '../shared/either'
 import { InvalidNameError } from './errors/invalid-name-error'
 import { InvalidEmailError } from './errors/invalid-email-error'
 import { Email } from './Email'
 import { Name } from './name'
 
 export class User {
-  static create (userData: UserData): Either<InvalidNameError | InvalidEmailError, User> {
-    const nameOrError = Name.create(userData.name)
-    if (nameOrError.isLeft()) {
-      return left(new InvalidNameError())
+    public readonly email: Email
+    public readonly name: Name
+
+    private constructor (name: Name, email: Email) {
+      this.name = name
+      this.email = email
     }
 
-    const emailOrError = Email.create(userData.email)
-    if (emailOrError.isLeft()) {
-      return left(new InvalidEmailError())
+    static create (userData: UserData): Either<InvalidNameError | InvalidEmailError, User> {
+      const nameOrError = Name.create(userData.name)
+      if (nameOrError.isLeft()) {
+        return left(new InvalidNameError())
+      }
+
+      const emailOrError = Email.create(userData.email)
+      if (emailOrError.isLeft()) {
+        return left(new InvalidEmailError())
+      }
+
+      const name: Name = nameOrError.value as Name
+      const email: Email = emailOrError.value as Email
+
+      return right(new User(name, email))
     }
-  }
 }
