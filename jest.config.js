16aee2a5e90120e8e259ffd0a89b87c141ae7c8f
Malu Silva <malursilva1712@gmail.com>
2021-01-14 19:47:36 -0300

    chore: improve jest config

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,14 @@
-/*
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/en/configuration.html
- */
 
 module.exports = {
   roots: ['<rootDir>/src'],
   coverageDirectory: 'coverage',
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!**/test/**',
+    '!**/config/**'
+  ],
   testEnvironment: 'node',
   transform: {
-    '.+\\.ts$': 'ts-test'
+    '.+\\.ts$': 'ts-jest'
   }
 }
