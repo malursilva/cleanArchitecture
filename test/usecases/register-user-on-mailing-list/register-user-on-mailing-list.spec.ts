2b299ad81b823120506dae152019ea2b34900593
Malu Silva <malursilva1712@gmail.com>
2021-02-03 22:29:56 -0300

    test: web controller should return 201 when user is created

--- test/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ test/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -6,7 +6,6 @@ import { InMemoryUserRepository } from '@test/usecases/register-user-on-mailing-
 describe('Register user on mailing list use case', () => {
   test('should add user with complete data to mailing list', async () => {
     const users: UserData[] = []
-
     const repo: UserRepository = new InMemoryUserRepository(users)
     const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
     const name = 'any_name'
