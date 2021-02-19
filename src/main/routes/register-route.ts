dd5271d028f74bebfb5b6116db3d624347f5256f
Malu Silva <malursilva1712@gmail.com>
2021-02-18 22:28:13 -0300

    refactor: move middleware and routes folders inside main folder

--- src/main/routes/register-route.ts
+++ src/main/routes/register-route.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { makeRegisterUserController } from '@/main/factories/'
-import { adaptRout } from '@/main/config/adapters/'
+import { adaptRout } from '@/main/adapters'
 
 export default (router: Router): void => {
   router.post('/register', adaptRout(makeRegisterUserController()))
