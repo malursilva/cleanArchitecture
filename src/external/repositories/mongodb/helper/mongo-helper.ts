3986c15d939222c5677f36c701b847b1c4df5b1e
Malu Silva <malursilva1712@gmail.com>
2021-02-25 22:51:44 -0300

    feat: mongodb helper module

---
+++ src/external/repositories/mongodb/helper/mongo-helper.ts
@@ -0,0 +1,27 @@
+import { connect } from 'http2'
+import { MongoClient, Collection } from 'mongodb'
+import { disconnect } from 'process'
+
+export const MongoHelper = {
+    client: null as MongoClient,
+
+    async connect (uri: string): Promise<void> {
+        this.client = await MongoClient.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+    },
+    
+    async disconnect (): Promise<void> {
+        this.client.close()
+    },
+
+    getCollection (name: string): Collection {
+        return this.client.db().collection(name)
+    },
+
+    clearCollection (name: string): void {
+        this.client.db().collection(name).deleteMany({})
+    }
+
+} 
