583d0d2c27a0d62d8d45b011f820cbdf9a5c79f0
Malu Silva <malursilva1712@gmail.com>
2021-02-25 23:51:16 -0300

    feat: add user after verify existence using mongodb

---
+++ src/external/repositories/mongodb/mongodb-user-repository.ts
@@ -0,0 +1,28 @@
+import { UserData } from '@/entities'
+import { UserRepository } from '@/usecases/register-user-on-mailing-list/ports'
+import { MongoHelper } from './helper'
+
+export class MongodbUserRepository implements UserRepository {
+  async add (user: UserData): Promise<void> {
+    const userCollection = MongoHelper.getCollection('users')
+    const exists = await this.exists(user)
+    if (!exists) {
+      await userCollection.insertOne(user)
+    }
+  }
+
+  async findUserByEmail (email: String): Promise<UserData> {
+    const userCollection = MongoHelper.getCollection('users')
+    const result = await userCollection.findOne({ email: email })
+    return result
+  }
+
+  findAllUsers (): Promise<UserData[]> {
+    throw new Error('Method not implemented.')
+  }
+
+  async exists (user: UserData): Promise<boolean> {
+    const result = await this.findUserByEmail(user.email)
+    return result != null
+  }
+}
